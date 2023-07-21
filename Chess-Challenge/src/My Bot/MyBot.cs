8c6f695205f3a506044d4261482bc194a45cc8fd
carlylemorris <carlylemorris@gmail.com>
2023-07-21 17:49:25 -0500

    First minibot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,22 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move[] moves = board.GetLegalMoves(true);
+
+        if(moves.Length == 0){
+            return RandoChoice(board.GetLegalMoves(false));
+        }else{
+            return RandoChoice(moves);
+        }
+        
+    }
+
+    private Move RandoChoice(Move[] moves){
+        Random rnd = new Random();
+        return moves[rnd.Next() % moves.Length];
     }
 }
