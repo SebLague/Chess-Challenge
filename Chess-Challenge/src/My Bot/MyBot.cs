c8d5d21d685882c892d09dfc175d960150c14815
Stefan Hoffmeister <hoffmeister.stefan99@gmail.com>
2023-07-21 20:57:05 +0200

    prio checkmate, prio2 convert, prio 3 schlagen, -> rng

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,80 @@
 ﻿using ChessChallenge.API;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
-public class MyBot : IChessBot
+namespace ChessChallenge.Example
 {
-    public Move Think(Board board, Timer timer)
+    // A simple bot that can spot mate in one, and always captures the most valuable piece it can.
+    // Plays randomly otherwise.
+    public class MyBot : IChessBot
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        // Piece values: null, pawn, knight, bishop, rook, queen, king
+        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+
+        public Move Think(Board board, Timer timer)
+        {
+            List<Move> allMoves = board.GetLegalMoves().ToList();
+
+            // Pick a random move to play if nothing better is found
+            Random rng = new();
+            Move moveToPlay = allMoves[rng.Next(allMoves.Count)];
+
+            //Priortize Checkmates
+            Move[] checkmates = allMoves.Where(m => MoveIsCheckmate(board, m)).ToArray();
+            if (checkmates.Length > 0)
+            {
+                return checkmates[0];
+            }
+
+            //Upgrade pieces to best piece only
+            Move[] promotions = allMoves.Where(m => m.IsPromotion).OrderByDescending(m => pieceValues[(int)m.PromotionPieceType]).ToArray();
+            if (promotions.Length > 0)
+            {
+                return promotions[0];
+            }
+
+            IOrderedEnumerable<Move> rankedMoves = allMoves
+                        .Where(m => !checkmates.Contains(m) && !promotions.Contains(m) && CalculateMoveValue(board, m) > 0)
+                        .OrderByDescending(m => CalculateMoveValue(board, m));
+
+            if(rankedMoves.Count() > 0) 
+            {
+                moveToPlay = rankedMoves.First();
+            }
+
+            return  moveToPlay;
+        }
+
+        // Test if this move gives checkmate
+        bool MoveIsCheckmate(Board board, Move move)
+        {
+            board.MakeMove(move);
+            bool isMate = board.IsInCheckmate();
+            board.UndoMove(move);
+            return isMate;
+        }
+
+        bool MoveCreatesTarget(Board board, Move move)
+        {
+            board.MakeMove(move);
+            Move[] enemyMoves = board.GetLegalMoves();
+            bool isTarget = enemyMoves.Any(m => m.TargetSquare == move.TargetSquare);
+            board.UndoMove(move);
+
+            return isTarget;
+        }
+
+
+        int CalculateMoveValue(Board board, Move move)
+        {
+            Piece capturedPiece = board.GetPiece(move.TargetSquare);
+            int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+
+            int cost = !MoveCreatesTarget(board, move) ? 0 : pieceValues[(int)move.MovePieceType];
+
+            return capturedPieceValue - cost;
+        }
     }
 }
