c5365e60a30bae6375c4bc3949b99dcfcb8ef66e
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2023-07-22 05:27:22 +0300

    1.1 - Basics for a searching engine

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,19 +1,112 @@
 ﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.Intrinsics.X86;
 using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
-    private Random _rng;
+    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 0 };
 
-    public MyBot()
+    private int Evaluate(Board board)
     {
-        _rng = new Random();
+        int score = 0;
+        for (var color = 0; color < 2; color++)
+        {
+            var isWhite = color == 0;
+            for (var piece = PieceType.Pawn; piece <= PieceType.King; piece++)
+            {
+                var pieceIndex = (int)piece;
+                var bitboard = board.GetPieceBitboard(piece, isWhite);
+
+                // Material evaluation
+                score += pieceValues[pieceIndex] * BitOperations.PopCount(bitboard);
+            }
+
+            score = -score;
+        }
+
+        if (!board.IsWhiteToMove)
+        {
+            score = -score;
+        }
+
+        return score;
+    }
+
+    public int Search(Board board, Timer timer, int totalTime, int depth, out Move bestMove)
+    {
+        bestMove = Move.NullMove;
+        if (depth == 0)
+        {
+            var score = Evaluate(board);
+            return score;
+        }
+
+        var moves = board.GetLegalMoves();
+        var bestScore = int.MinValue;
+        var movesEvaluated = 0;
+
+        // Loop over each legal move
+        foreach (var move in moves)
+        {
+            // If we are out of time, stop searching
+            if (timer.MillisecondsElapsedThisTurn * 30 > totalTime)
+            {
+                return bestScore;
+            }
+
+            board.MakeMove(move);
+            var score = -Search(board, timer, totalTime, depth - 1, out _);
+            board.UndoMove(move);
+
+            // If the move is better than our current best, update our best move
+            if (score > bestScore)
+            {
+                bestScore = score;
+                bestMove = move;
+            }
+
+            // Count the number of moves we have evaluated for detecting mates and stalemates
+            movesEvaluated++;
+        }
+
+        if (movesEvaluated == 0)
+        {
+            if (board.IsInCheck())
+            {
+                // Checkmate
+                return -1000000;
+            }
+            else
+            {
+                // Stalemate
+                return 0;
+            }
+        }
+
+        return bestScore;
     }
 
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        var moveIndex = _rng.Next(moves.Length);
-        return moves[moveIndex];
+        var totalTime = timer.MillisecondsRemaining;
+        var bestMove = Move.NullMove;
+        // Iterative deepening
+        for (var depth = 1; depth < 128; depth++)
+        {
+            var score = Search(board, timer, totalTime, depth, out var move);
+
+            // If we are out of time, we cannot trust the move that was found during this iteration
+            if (timer.MillisecondsElapsedThisTurn * 30 > totalTime)
+            {
+                break;
+            }
+
+            bestMove = move;
+            Console.WriteLine($"{score} {move}");
+        }
+
+        return bestMove;
     }
 }
--- SUBMISSION-CHANGELOG.md
+++ SUBMISSION-CHANGELOG.md
@@ -4,9 +4,33 @@ Initial release - randoom legal mover
 53 tokens
 
 ```
-Score of ChessChallenge vs ChessChallengeEvil: 2 - 4211 - 787  [0.079] 5000
-...      ChessChallenge playing White: 2 - 2081 - 416  [0.084] 2499
-...      ChessChallenge playing Black: 0 - 2130 - 371  [0.074] 2501
+Score of ChessChallenge1.0 vs ChessChallengeEvil: 2 - 4211 - 787  [0.079] 5000
+...      ChessChallenge1.0 playing White: 2 - 2081 - 416  [0.084] 2499
+...      ChessChallenge1.0 playing Black: 0 - 2130 - 371  [0.074] 2501
 ...      White vs Black: 2132 - 2081 - 787  [0.505] 5000
 Elo difference: -426.4 +/- 12.1, LOS: 0.0 %, DrawRatio: 15.7 %
+```
+
+### 1.1
+Basics for a searching engine:
+* Iterative deepening
+* Brute-force search
+* Basic time tracking
+* Material-only evaluation
+* Mate and stalemate detection
+
+329 tokens
+
+```
+Score of ChessChallenge1.1 vs ChessChallenge1.0: 1783 - 0 - 217  [0.946] 2000
+...      ChessChallenge1.1 playing White: 886 - 0 - 114  [0.943] 1000
+...      ChessChallenge1.1 playing Black: 897 - 0 - 103  [0.949] 1000
+...      White vs Black: 886 - 897 - 217  [0.497] 2000
+Elo difference: 496.6 +/- 23.2, LOS: 100.0 %, DrawRatio: 10.8 %
+
+Score of ChessChallenge1.1 vs ChessChallengeEvil: 1667 - 0 - 333  [0.917] 2000
+...      ChessChallenge1.1 playing White: 828 - 0 - 171  [0.914] 999
+...      ChessChallenge1.1 playing Black: 839 - 0 - 162  [0.919] 1001
+...      White vs Black: 828 - 839 - 333  [0.497] 2000
+Elo difference: 416.7 +/- 18.6, LOS: 100.0 %, DrawRatio: 16.7 %
 ```
