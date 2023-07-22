66970dd3dee40897c1c05458c346cc0e3681b1e1
lucametehau <31002793+lucametehau@users.noreply.github.com>
2023-07-23 02:14:59 +0300

    TT Move Ordering (#8)
    * First commit
    
    * Add basic TT move ordering
    
    Additionally shave some token
    
    Tokens: 582

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,25 +5,32 @@ public class MyBot : IChessBot
 {
     Move bestmoveRoot = Move.NullMove;
     ulong[] hashStack = new ulong[1000];
+    int[] pieceVal = {0, 100, 310, 330, 500, 1000};
+
+    struct TTEntry {
+        ulong key;
+        public Move move;
+
+        public TTEntry(ulong _key, Move _move) {key = _key; move = _move; }
+    }
+
+    const int entries = (1 << 18);
+    ulong nodes = 0;
+    TTEntry[] tt = new TTEntry[entries];
 
     public int Evaluate(Board board) {
         int eval = 0;
-        eval += 100 * (board.GetPieceList(PieceType.Pawn, true).Count - board.GetPieceList(PieceType.Pawn, false).Count);
-
-        eval += 310 * (board.GetPieceList(PieceType.Knight, true).Count - board.GetPieceList(PieceType.Knight, false).Count);
-        
-        eval += 330 * (board.GetPieceList(PieceType.Bishop, true).Count - board.GetPieceList(PieceType.Bishop, false).Count);
-
-        eval += 500 * (board.GetPieceList(PieceType.Rook, true).Count - board.GetPieceList(PieceType.Rook, false).Count);
-
-        eval += 1000 * (board.GetPieceList(PieceType.Queen, true).Count - board.GetPieceList(PieceType.Queen, false).Count);
+        for(var p = PieceType.Pawn; p <= PieceType.Queen; p++)
+            eval += pieceVal[(int)p] * (board.GetPieceList(p, true).Count - board.GetPieceList(p, false).Count);
 
         if(!board.IsWhiteToMove)
             eval *= -1;
         return eval;
     }
     public int Search(Board board, Timer timer, int alpha, int beta, int depth, int ply) {
-        hashStack[board.PlyCount] = board.ZobristKey;
+        nodes++;
+        ulong key = board.ZobristKey;
+        hashStack[board.PlyCount] = key;
         bool qsearch = (depth <= 0);
         int best = -30000;
 
@@ -40,12 +47,16 @@ public class MyBot : IChessBot
             if(best > alpha) alpha = best;
         }
 
+        TTEntry entry = tt[key % entries];
         Move[] moves = board.GetLegalMoves(qsearch);
         int[] scores = new int[moves.Length];
-        for(int i = 0; i < moves.Length; i++)
-            scores[i] = 100 * moves[i].TargetSquare.Index - moves[i].StartSquare.Index;
         for(int i = 0; i < moves.Length; i++) {
-            if(depth > 2 && timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 30) return 0;
+            if(moves[i] == entry.move) scores[i] = 1000000;
+            else scores[i] = 100 * moves[i].TargetSquare.Index - moves[i].StartSquare.Index;
+        }
+        Move bestMove = Move.NullMove;
+        for(int i = 0; i < moves.Length; i++) {
+            if(timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 30) return 0;
 
             int ind = i;
             for(int j = i + 1; j < moves.Length; j++) {
@@ -60,6 +71,7 @@ public class MyBot : IChessBot
             board.UndoMove(move);
             if(score > best) {
                 best = score;
+                bestMove = move;
                 if(ply == 0)
                     bestmoveRoot = move;
                 if(score > alpha) {
@@ -72,6 +84,7 @@ public class MyBot : IChessBot
             if(board.IsInCheck()) return -30000 + ply;
             else return 0;
         }
+        tt[key % entries] = new TTEntry(key, bestMove);
         return best;
     }
     public Move Think(Board board, Timer timer)
@@ -81,7 +94,7 @@ public class MyBot : IChessBot
             int score = Search(board, timer, -30000, 30000, depth, 0);
             if(timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 30)
                 break;
-            Console.WriteLine($"info depth {depth} score cp {score} time {timer.MillisecondsElapsedThisTurn} pv {bestmoveRoot}");
+            Console.WriteLine($"info depth {depth} score cp {score} time {timer.MillisecondsElapsedThisTurn} pv {bestmoveRoot} nodes {nodes} nps {nodes * 1000 / (ulong)timer.MillisecondsElapsedThisTurn}");
             bestMove = bestmoveRoot;
         }
         return bestMove;
