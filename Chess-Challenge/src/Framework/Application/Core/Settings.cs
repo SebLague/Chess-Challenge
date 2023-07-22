8ed36b92054fa953400c59e01eee66e572386df5
Sebastian Lague <sebastlague@gmail.com>
2023-07-22 13:42:16 +0200

    Bump minor version (1.11)

--- Chess-Challenge/src/Framework/Application/Core/Settings.cs
+++ Chess-Challenge/src/Framework/Application/Core/Settings.cs
@@ -4,7 +4,7 @@ namespace ChessChallenge.Application
 {
     public static class Settings
     {
-        public const string Version = "1.1";
+        public const string Version = "1.11";
 
         // Game settings
         public const int GameDurationMilliseconds = 60 * 1000;
