dc0e1681a544ab5c753cf85a05ea09106e239287
kato-daichi <firefather@telenet.be>
2023-07-24 00:39:07 +0200

    Update MatchStatsUI.cs
    add match points & win %

--- Chess-Challenge/src/Framework/Application/UI/MatchStatsUI.cs
+++ Chess-Challenge/src/Framework/Application/UI/MatchStatsUI.cs
@@ -27,8 +27,16 @@ namespace ChessChallenge.Application
 
                 void DrawStats(ChallengeController.BotMatchStats stats)
                 {
+                    int numgames = stats.NumWins + stats.NumDraws + stats.NumLosses;
+                    if (numgames == 0)
+                        numgames = 1;
+                    double winpercent = (double)stats.NumWins / numgames * 100;
+                    double points = (double)stats.NumWins + (double)stats.NumDraws/2;
+
                     DrawNextText(stats.BotName + ":", nameFontSize, Color.WHITE);
-                    DrawNextText($"Score: +{stats.NumWins} ={stats.NumDraws} -{stats.NumLosses}", regularFontSize, col);
+                    DrawNextText($"Result: +{stats.NumWins} ={stats.NumDraws} -{stats.NumLosses}", regularFontSize, col);
+                    DrawNextText($"Score: {points.ToString("0.0")}/{numgames}", regularFontSize, col);
+                    DrawNextText($"Win%: {winpercent.ToString("0.00")}", regularFontSize, col);
                     DrawNextText($"Num Timeouts: {stats.NumTimeouts}", regularFontSize, col);
                     DrawNextText($"Num Illegal Moves: {stats.NumIllegalMoves}", regularFontSize, col);
                 }
