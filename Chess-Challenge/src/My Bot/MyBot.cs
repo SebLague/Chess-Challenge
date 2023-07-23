e8b94e7c71ff7aefe4296f96966d509cd3b63897
r2dev2 <ronak.badhe@gmail.com>
2023-07-23 14:38:09 -0700

    can beat evilbot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,16 +1,18 @@
 ﻿using ChessChallenge.API;
 using System;
+using System.Linq;
 
 public class MyBot : IChessBot
 {
     // Piece values: null, pawn, knight, bishop, rook, queen, king
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    Random rng = new();
 
     public Move Think(Board board, Timer timer)
     {
         int score = 0;
 
-        Move[] moves = board.GetLegalMoves();
+        Move[] moves = GetMoves(board);
         Move retMove = moves[0];
 
         foreach (Move move in moves)
@@ -18,17 +20,25 @@ public class MyBot : IChessBot
             board.MakeMove(move);
             int newScore = AlphaBetaMax(board, int.MinValue, int.MaxValue, 4);
             board.UndoMove(move);
-            if (newScore > score)
+            if (newScore < score)
             {
                 score = newScore;
                 retMove = move;
             }
         }
+        Console.WriteLine(score);
         return retMove;
+    }
+
+    Move[] GetMoves(Board board)
+    {
+        return board.GetLegalMoves().OrderBy(x => rng.Next()).ToArray();
     }
 
     int Evaluate(Board board)
     {
+        if (board.IsInCheckmate()) return -10000;
+        if (board.IsDraw()) return 0;
         int score = 0;
         PieceList[] pieceLists = board.GetAllPieceLists();
         for (int i = 0; i < 5; i++)
@@ -36,7 +46,7 @@ public class MyBot : IChessBot
             int val = pieceValues[i + 1];
             score += (pieceLists[i].Count - pieceLists[i + 6].Count) * val;
         }
-        return score * (board.IsWhiteToMove ? -1 : 1);
+        return score * (board.IsWhiteToMove ? 1 : -1);
     }
 
     int AlphaBetaMax(Board board, int alpha, int beta, int depth_left)
