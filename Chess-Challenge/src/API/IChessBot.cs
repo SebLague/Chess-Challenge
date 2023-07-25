055b9e8615d6049bd52021740111bbd923e936ea
Paul Spooner <dudecon@hotmail.com>
2023-07-25 11:05:32 -0700

    End of game method
    Added a bot method called at the end of each game

--- Chess-Challenge/src/API/IChessBot.cs
+++ Chess-Challenge/src/API/IChessBot.cs
@@ -4,5 +4,11 @@ namespace ChessChallenge.API
     public interface IChessBot
     {
         Move Think(Board board, Timer timer);
+
+        void GameOver(Board board)
+        {
+            // Override for end-of-game operations like machine learning training
+        }
+        
     }
 }
