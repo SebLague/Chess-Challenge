4186bd92f7814e20ef5a5490fd60aab5a7965cb7
Moonwalker316 <120405376+Moonwalker316@users.noreply.github.com>
2023-07-26 08:33:42 +0100

    Update Settings.cs

--- Chess-Challenge/src/Framework/Application/Core/Settings.cs
+++ Chess-Challenge/src/Framework/Application/Core/Settings.cs
@@ -9,7 +9,7 @@ namespace ChessChallenge.Application
         // Game settings
         public const int GameDurationMilliseconds = 60 * 1000;
         public const float MinMoveDelay = 0;
-        public static readonly bool RunBotsOnSeparateThread = false; // IF NOT IN FAST FORWARD, TURN THIS ON
+        public static bool RunBotsOnSeparateThread = false; // IF NOT IN FAST FORWARD, TURN THIS ON - It's no longer readonly
 
         // Display settings
         public const bool DisplayBoardCoordinates = true;
