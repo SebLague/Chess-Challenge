28cf3511a201819831517492781e0ab091393a73
britto <josebrittosaaji@gmail.com>
2023-07-27 17:29:37 -0400

    Remove Room Id by default

--- Chess-Challenge/src/Evil Bot/NetworkedBot.cs
+++ Chess-Challenge/src/Evil Bot/NetworkedBot.cs
@@ -12,7 +12,7 @@ namespace ChessChallenge.Example;
 
 public class NetworkedBot : IChessBot
 {
-    public const string ROOM_ID = "Test Room";
+    public const string ROOM_ID = "";
     
     
     public const string PROTOCOL_VERSION = "0.1";
