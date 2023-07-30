988db4d37aa7703cd30f5aee60c5484ee3804f11
Steven Good <segood@sep.com>
2023-07-30 14:03:15 -0400

    WIP

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -16,6 +16,7 @@ public class Edge : IComparable<Edge>
         this.move = move;
         this.node = node;
     }
+
     public int CompareTo(Edge? other)
     {
         return other.node.moveStrength.CompareTo(this.node.moveStrength);
@@ -24,34 +25,39 @@ public class Edge : IComparable<Edge>
 
 public class Node
 {
-    public static int nodeCount = 0;
-    public Edge? bestMove { get; set; } = null;
-    public int moveStrength { get; set; }
-    //public ulong? position { get; set; } = null;
-    public List<Edge>? edges { get; set; } = null;
+    public static int nodeCount;
 
     public Node(int moveStrength)
     {
         nodeCount++;
         this.moveStrength = moveStrength;
     }
+
+    public Edge? bestMove { get; set; }
+
+    public int moveStrength { get; set; }
+
+    //public ulong? position { get; set; } = null;
+    public List<Edge>? edges { get; set; }
 }
 
 public class MyBot : IChessBot
 {
     private readonly int[] _pieceValues = { 0, 100, 320, 300, 500, 900, 50000 };
 
-    private Node? _root = null;
+    private readonly int _bigNumber = Int32.MaxValue / 10;
 
-    private bool _isWhite = false;
+    private bool _isWhite;
 
-    private int _bigNumber = Int32.MaxValue / 10;
+    private Node? _root;
 
-    private int cutOffCounter = 0;
+    private int cutOffCounter;
 
     public Move Think(Board board, Timer timer)
     {
-        //_root = null;
+        //var rook = board.GetPieceList(PieceType.Rook, _isWhite)[0];
+        BitboardHelper.VisualizeBitboard(BitboardHelper.GetPieceAttacks(PieceType.Rook, new Square("a1"), board), true);
+
         if (_root == null)
         {
             _isWhite = board.IsWhiteToMove;
@@ -62,32 +68,30 @@ public class MyBot : IChessBot
             var lastMove = board.GameMoveHistory[^1];
             var chosenEdge = _root.edges?.Where(edge => edge.move.Equals(lastMove))?.First();
             if (chosenEdge != null)
-            {
                 _root = chosenEdge.node;
-            }
             else
-            {
                 _root = new Node(_isWhite ? Int32.MinValue : Int32.MaxValue);
-            }
         }
 
         var depth = 0;
+
 
         while (timer.MillisecondsElapsedThisTurn < 500)
         {
             if (depth >= 5)
                 break;
             Search(_root, depth, Int32.MinValue, Int32.MaxValue, board);
-           depth++;
+            depth++;
         }
+
         //EvilSearch(board, _evilRoot, 4);
         var ourMove = _root.bestMove;
         _root = ourMove.node;
-        Console.WriteLine($"Percentage {(float)cutOffCounter/(float)Node.nodeCount}, Depth {depth - 1}");
+        //Console.WriteLine($"Percentage {cutOffCounter / (float)Node.nodeCount}, Depth {depth - 1}");
         return ourMove.move;
     }
 
-    void Search(Node node, int depth, int alpha, int beta, Board board)
+    private void Search(Node node, int depth, int alpha, int beta, Board board)
     {
         if (depth == 0)
         {
@@ -96,9 +100,8 @@ public class MyBot : IChessBot
         }
 
         if (node.edges == null)
-        {
-            node.edges = board.GetLegalMoves().Select(move => new Edge(move, new Node(!board.IsWhiteToMove ? -_bigNumber : _bigNumber))).ToList();
-        }
+            node.edges = board.GetLegalMoves()
+                .Select(move => new Edge(move, new Node(!board.IsWhiteToMove ? -_bigNumber : _bigNumber))).ToList();
 
         if (node.edges.Count == 0)
         {
@@ -106,12 +109,9 @@ public class MyBot : IChessBot
             return;
         }
 
-        node.moveStrength = board.IsWhiteToMove ? Int32.MinValue : Int32.MaxValue;
+        node.moveStrength = board.IsWhiteToMove ? int.MinValue : int.MaxValue;
 
-        if (!board.IsWhiteToMove)
-        {
-            node.edges.Reverse();
-        }
+        if (!board.IsWhiteToMove) node.edges.Reverse();
 
         foreach (var edge in node.edges)
         {
@@ -127,6 +127,7 @@ public class MyBot : IChessBot
                     node.moveStrength = edge.node.moveStrength;
                     node.bestMove = edge;
                 }
+
                 alpha = Math.Max(alpha, node.moveStrength);
                 if (node.moveStrength >= beta)
                 {
@@ -142,6 +143,7 @@ public class MyBot : IChessBot
                     node.moveStrength = edge.node.moveStrength;
                     node.bestMove = edge;
                 }
+
                 beta = Math.Min(beta, node.moveStrength);
                 if (node.moveStrength <= alpha)
                 {
@@ -158,40 +160,9 @@ public class MyBot : IChessBot
         //node.moveStrength = board.IsWhiteToMove ? node.edges.First().node.moveStrength : node.edges.Last().node.moveStrength;
     }
 
-    void EvilSearch(Board board, Node node, int depth)
+    private int EvaluatePosition(Board board)
     {
-        if (depth == 0)
-        {
-            node.moveStrength = EvaluatePosition(board);
-            return;
-        }
-
-        if (node.edges == null)
-        {
-            node.edges = board.GetLegalMoves().Select(move => new Edge(move, new Node(!board.IsWhiteToMove ? -_bigNumber : _bigNumber))).ToList();
-        }
-
-        if (node.edges.Count == 0)
-        {
-            node.moveStrength = EvaluatePosition(board);
-            return;
-        }
-
-        foreach (var edge in node.edges)
-        {
-            board.MakeMove(edge.move);
-            EvilSearch(board, edge.node, depth - 1);
-            board.UndoMove(edge.move);
-        }
-
-        node.edges.Sort();
-
-        node.moveStrength = board.IsWhiteToMove ? node.edges.First().node.moveStrength : node.edges.Last().node.moveStrength;
-    }
-
-    int EvaluatePosition(Board board)
-    {
-        int evaluation = 0;
+        var evaluation = 0;
         if (board.IsInCheckmate())
         {
             evaluation = board.IsWhiteToMove ? -_bigNumber : _bigNumber;
@@ -206,12 +177,50 @@ public class MyBot : IChessBot
 
             foreach (var pieceList in allPieceLists)
             {
-                int pieceListValue = pieceList.Count * _pieceValues[(int)pieceList.TypeOfPieceInList];
-
-                if (!pieceList.IsWhitePieceList)
+                var pieceListValue = pieceList.Count * _pieceValues[(int)pieceList.TypeOfPieceInList];
+                
+                switch (pieceList.TypeOfPieceInList)
+                
                 {
-                    pieceListValue *= -1;
+                    case PieceType.Pawn:
+                        foreach (var pawn in pieceList)
+                        {
+                            var rank = pawn.Square.Rank;
+                            pieceListValue += pieceList.IsWhitePieceList
+                                ? rank * rank
+                                : (7 - rank) * (7 - rank);
+
+                            var file = pawn.Square.Rank;
+                            if (board.GetPiece(new Square(Math.Max(0, file - 1), rank - 1)).IsPawn ||
+                                board.GetPiece(new Square(Math.Min(7, file + 1), rank - 1)).IsPawn)
+                            {
+                                pieceListValue += 25;
+                            }
+                        }   
+                        break;
+                    case PieceType.Knight:
+                        foreach (var knight in pieceList)
+                        {
+                            var file = 2 * knight.Square.File - 7;
+                            var rank = 2 * knight.Square.Rank - 7;
+                            pieceListValue += 50 - 10 * (int)Math.Sqrt((rank * rank) + (file * file));
+                        }
+                        break;
+                    case PieceType.Bishop:
+                        foreach (var bishop in pieceList)
+                        {
+                            pieceListValue += 15 * (2 - Math.Min(Math.Abs(bishop.Square.Rank - bishop.Square.File), Math.Abs(bishop.Square.Rank + bishop.Square.File)));
+                        }
+                        break;
+                    case PieceType.Rook:
+                        foreach (var bishop in pieceList)
+                        {
+                            //pieceListValue += 15 * (2 - Math.Min(Math.Abs(bishop.Square.Rank - bishop.Square.File), Math.Abs(bishop.Square.Rank + bishop.Square.File)));
+                        }
+                        break;
                 }
+
+                if (!pieceList.IsWhitePieceList) pieceListValue *= -1;
 
                 evaluation += pieceListValue;
             }
@@ -221,5 +230,10 @@ public class MyBot : IChessBot
         //evaluation += rng.Next(100) - 50;
 
         return evaluation;
+    }
+
+    bool RookIsNotBlockedByItsOwnPawn(Piece rook)
+    {
+        return true;
     }
 }
