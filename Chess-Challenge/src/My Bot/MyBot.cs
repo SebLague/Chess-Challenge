1689496f5fbc809e7a9b6eb1952f782eadc03530
Sn00pyW00dst0ck <gabealdous@gmail.com>
2023-08-15 14:44:27 -0500

    Added piece position tables

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -20,6 +20,71 @@ public class MyBot : IChessBot
         }
     }
 
+    private static readonly double[,] TABLES =
+    { 
+        {
+            0, 0, 0, 0, 0, 0, 0, 0,
+            50, 50, 50, 50, 50, 50, 50, 50,
+            10, 10, 20, 30, 30, 20, 10, 10,
+            5, 5, 10, 25, 25, 10, 5, 5,
+            0, 0, 0, 20, 20, 0, 0, 0,
+            5, -5, -10, 0, 0, -10, -5, 5,
+            5, 10, 10, -20, -20, 10, 10, 5,
+            0, 0, 0, 0, 0, 0, 0, 0
+        },
+        {
+            -50,-40,-30,-30,-30,-30,-40,-50,
+            -40,-20,  0,  5,  5,  0,-20,-40,
+            -30,  0, 10, 15, 15, 10,  0,-30,
+            -30,  5, 15, 20, 20, 15,  5,-30,
+            -30,  0, 15, 20, 20, 15,  0,-30,
+            -30,  5, 10, 15, 15, 10,  5,-30,
+            -40,-20,  0,  5,  5,  0,-20,-40,
+            -50,-40,-30,-30,-30,-30,-40,-50
+        },
+        {
+            -20,-10,-10,-10,-10,-10,-10,-20,
+            -10,  0,  0,  0,  0,  0,  0,-10,
+            -10,  0,  5, 10, 10,  5,  0,-10,
+            -10,  5,  5, 10, 10,  5,  5,-10,
+            -10,  0, 10, 10, 10, 10,  0,-10,
+            -10, 10, 10, 10, 10, 10, 10,-10,
+            -10,  5,  0,  0,  0,  0,  5,-10,
+            -20,-10,-10,-10,-10,-10,-10,-20
+        },
+        {
+            0,  0,  0,  0,  0,  0,  0,  0,
+            5, 10, 10, 10, 10, 10, 10,  5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            0,  0,  0,  5,  5,  0,  0,  0
+        },
+        {
+            -20,-10,-10, -5, -5,-10,-10,-20,
+            -10,  0,  0,  0,  0,  0,  0,-10,
+            -10,  0,  5,  5,  5,  5,  0,-10,
+            -5,  0,  5,  5,  5,  5,  0, -5,
+            0,  0,  5,  5,  5,  5,  0, -5,
+            -10,  5,  5,  5,  5,  5,  0,-10,
+            -10,  0,  5,  0,  0,  0,  0,-10,
+            -20,-10,-10, -5, -5,-10,-10,-20
+        },
+        {
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0
+        }
+    };
+
+
     public Move Think(Board board, Timer timer)
     {
         EvaluatedMove best = NegaMax(board, 5, double.NegativeInfinity, double.PositiveInfinity);
@@ -55,20 +120,23 @@ public class MyBot : IChessBot
 
     private static double Evaluate(Board board)
     {
-        int evaluatedScore = 0;
+        double evaluatedScore = 0;
 
         // Check end conditions
         if (board.IsDraw()) { return evaluatedScore; }
-        if (board.IsInCheckmate()) { return board.IsWhiteToMove ? -10000000 : 10000000; }
+        if (board.IsInCheckmate()) { return board.IsWhiteToMove ? Double.NegativeInfinity: Double.PositiveInfinity; }
 
         PieceList[] pieces = board.GetAllPieceLists();
 
-        // Use a weighted piece differential
-        // TODO: Change to use pice locations and: pieces[i].GetPiece(j).Square.Index;
+        // Use a weighted piece differential & also add lookup table values
         int[] values = { 94, 281, 297, 512, 1025, 100000 };
         for (int i = 0; i < 6; i++)
         {
             evaluatedScore += (pieces[i].Count - pieces[i + 6].Count) * values[i];
+            foreach (Piece white in pieces[i])
+                evaluatedScore += TABLES[i, white.Square.Index];
+            foreach (Piece black in pieces[i])
+                evaluatedScore -= TABLES[i, 63 - black.Square.Index];
         }
 
         // Check if a player is in check
