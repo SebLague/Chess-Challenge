fede8ba3475ff100542413ba4bde193ccb0b3852
Sn00pyW00dst0ck <gabealdous@gmail.com>
2023-08-15 16:57:00 -0500

    Initial transposition table

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,5 +1,7 @@
 ﻿using ChessChallenge.API;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
+using System.Collections.Generic;
 
 public class MyBot : IChessBot
 {
@@ -19,7 +21,8 @@ public class MyBot : IChessBot
             return Eval.CompareTo(other.Eval);
         }
     }
-
+    
+    // TODO: Implement generation for these to reduce the token count (or convert to Neural Net)
     private static readonly double[,] TABLES =
     { 
         {
@@ -71,36 +74,58 @@ public class MyBot : IChessBot
             -10,  5,  5,  5,  5,  5,  0,-10,
             -10,  0,  5,  0,  0,  0,  0,-10,
             -20,-10,-10, -5, -5,-10,-10,-20
-        },
-        {
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0
         }
     };
+    private static readonly int MAX_DEPTH = 6;
 
+    // TODO: Transposition Tables (Zobrist Key -> { depth, flag, move })
+    // flag: 0 -> LOWERBOUND, 1 -> EXACT, 2 -> UPPERBOUND
+    private readonly Dictionary<ulong, Tuple<int, int, EvaluatedMove>> transpositions = new Dictionary<ulong, Tuple<int, int, EvaluatedMove>>();
 
     public Move Think(Board board, Timer timer)
     {
-        EvaluatedMove best = NegaMax(board, 5, double.NegativeInfinity, double.PositiveInfinity);
-        best.Eval *= -1;
+        EvaluatedMove best = new(default, 0);
+        // TODO: add a method to check for time constraints...
+        for (int depth = 1; depth <= MAX_DEPTH; depth++)
+        {
+            best = NegaMax(board, depth, double.NegativeInfinity, double.PositiveInfinity);
+            best.Eval *= -1;
+            Console.WriteLine(depth);
+        }
+        
         return best.Move;
     }
     
 
     private EvaluatedMove NegaMax(Board board, int depth, double alpha, double beta)
     {
+        double alphaOrig = alpha;
+        // Perform transposition lookup
+        if (transpositions.ContainsKey(board.ZobristKey) && transpositions[board.ZobristKey].Item1 >= depth)
+        {
+            if (transpositions[board.ZobristKey].Item2 == 0)
+            {
+                alpha = Math.Max(alpha, transpositions[board.ZobristKey].Item3.Eval);
+            }
+            else if (transpositions[board.ZobristKey].Item2 == 2)
+            {
+                beta = Math.Min(beta, transpositions[board.ZobristKey].Item3.Eval);
+            }
+
+            if ((transpositions[board.ZobristKey].Item2 == 1) || alpha >= beta)
+            {
+                return transpositions[board.ZobristKey].Item3;
+            }
+        }
+
+
         // Check terminal node
         if (depth == 0 || board.IsInCheckmate() || board.IsDraw()) { return new(default, Evaluate(board)); }
 
         Move[] moves = board.GetLegalMoves();
-        // Order moves here
+        // Order moves here?? 
 
+        // Perform Negamax with alpha beta pruning
         EvaluatedMove best = new(default, int.MinValue);
         foreach (Move move in moves)
         {
@@ -114,10 +139,13 @@ public class MyBot : IChessBot
             alpha = Math.Max(alpha, best.Eval);
             if (alpha >= beta) { break; }
         }
+
+        // Perform transposition storage
+        transpositions[board.ZobristKey] = new(depth, (best.Eval >= beta ? 2 : (best.Eval <= alphaOrig) ? 0 : 1), best);
         return best;
     }
 
-
+    // TODO: Add more evaluation metrics OR train a Neural Net to evaluate positions
     private static double Evaluate(Board board)
     {
         double evaluatedScore = 0;
@@ -129,8 +157,8 @@ public class MyBot : IChessBot
         PieceList[] pieces = board.GetAllPieceLists();
 
         // Use a weighted piece differential & also add lookup table values
-        int[] values = { 94, 281, 297, 512, 1025, 100000 };
-        for (int i = 0; i < 6; i++)
+        int[] values = { 94, 281, 297, 512, 1025 };
+        for (int i = 0; i < 5; i++)
         {
             evaluatedScore += (pieces[i].Count - pieces[i + 6].Count) * values[i];
             foreach (Piece white in pieces[i])
