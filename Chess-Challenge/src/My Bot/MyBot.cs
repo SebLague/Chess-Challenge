514736fda522b9314959d2b00215b2cbdb5b05db
Sn00pyW00dst0ck <gabealdous@gmail.com>
2023-08-15 10:54:55 -0500

    Somewhat optimized NegaMax

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,16 +5,16 @@ public class MyBot : IChessBot
 {
     private class EvaluatedMove : IComparable<EvaluatedMove>
     {
-        public EvaluatedMove(Move move, int eval)
+        public EvaluatedMove(Move move, double eval)
         {
             Move = move;
             Eval = eval;
         }
 
         public Move Move { get; set; }
-        public int Eval { get; set; }
+        public double Eval { get; set; }
 
-        public int CompareTo(EvaluatedMove? other)
+        int IComparable<EvaluatedMove>.CompareTo(EvaluatedMove? other)
         {
             return Eval.CompareTo(other.Eval);
         }
@@ -22,31 +22,13 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-
-        Move bestMove = moves[0];
-        int bestMoveEvaluation = int.MinValue;
-        foreach (Move move in moves)
-        {
-            board.MakeMove(move);
-
-            // Evaluate the board
-            EvaluatedMove evaluation = NegaMax(board, 3, int.MinValue, int.MaxValue);
-            Console.WriteLine(-evaluation.Eval);
-            if (-evaluation.Eval > bestMoveEvaluation)
-            {
-                bestMove = move;
-                bestMoveEvaluation = -evaluation.Eval;
-            }
-
-            board.UndoMove(move);
-        }
-        Console.WriteLine("Best: " + bestMoveEvaluation);
-        return bestMove;
+        EvaluatedMove best = NegaMax(board, 5, double.NegativeInfinity, double.PositiveInfinity);
+        best.Eval *= -1;
+        return best.Move;
     }
+    
 
-
-    private EvaluatedMove NegaMax(Board board, int depth, int alpha, int beta)
+    private EvaluatedMove NegaMax(Board board, int depth, double alpha, double beta)
     {
         // Check terminal node
         if (depth == 0 || board.IsInCheckmate() || board.IsDraw()) { return new(default, Evaluate(board)); }
@@ -58,7 +40,8 @@ public class MyBot : IChessBot
         foreach (Move move in moves)
         {
             board.MakeMove(move);
-            EvaluatedMove temp = NegaMax(board, depth - 1, alpha, beta);
+            EvaluatedMove temp = NegaMax(board, depth - 1, -beta, -alpha);
+            temp.Move = move;
             temp.Eval *= -1;
             board.UndoMove(move);
 
@@ -70,7 +53,7 @@ public class MyBot : IChessBot
     }
 
 
-    private static int Evaluate(Board board)
+    private static double Evaluate(Board board)
     {
         int evaluatedScore = 0;
 
