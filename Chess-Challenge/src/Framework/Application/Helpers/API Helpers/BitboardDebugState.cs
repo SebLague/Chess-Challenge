cfab8aa49085af10b28579799aab3fb2e5e5590b
Chris Pilcher <chris@bauxite.tech>
2023-12-22 13:07:29 -0500

    Remove Old Challenge Code

--- Chess-Challenge/src/Framework/Application/Helpers/API Helpers/BitboardDebugState.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ChessChallenge.Application.APIHelpers
-{
-    public static class BitboardDebugState
-    {
-        public static bool BitboardDebugVisualizationRequested { get; set; }
-        public static ulong BitboardToVisualize {get; set;}
-    }
-}
